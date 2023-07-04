c2ce5890ca33138a1dddb3bffc54c8d39ea50d87
Emad <emadul.islam@hotmail.co.uk>
2023-07-04 23:37:49 +0100

    adding toggletodo function

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import { prisma } from "../db";
 import { redirect } from "next/navigation";
+
 async function createTodo(data: FormData) {
   "use server";
-
   const title = data.get("title")?.valueOf();
   if (typeof title !== "string" || title.length === 0) {
     throw new Error("Invalid title");
   }
-
   await prisma.todo.create({ data: { title, complete: false } });
   redirect("/");
 }
