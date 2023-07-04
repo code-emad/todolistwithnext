d667715d3c125e10dea8545debd677735d6459b5
Emad <emadul.islam@hotmail.co.uk>
2023-07-04 23:16:48 +0100

    set up of new page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,18 @@
-export default function New() {
+import Link from "next/link"
 
-    return <>
-      <h1 className="text-2xl">New</h1>
-    <ul></ul>
+export default function New() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form className="flex gap-2 flex-col">
+        <input name="title" type="text" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"></input>
+        <div className="flex gap-1 justify-end">
+           <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Cancel</Link>
+           <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Create</button>
+        </div>
+      </form>
     </>
-  }
+  );
+}
